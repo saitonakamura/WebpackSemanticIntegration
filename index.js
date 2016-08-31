01ad69f1cd85f76a5214e97fc8ba61fde37df300
Saito Nakamura <saitonakamuramb@gmail.com>
2016-09-01 01:16:56 +0300

    version with error 'jQuery is not defined'

---
+++ index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import semantic from 'semantic-ui'
+
+$('#dropdown').dropdown()
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+     entry: './index.js',
+     output: {
+         path: path.resolve(__dirname, "build"),
+         filename: 'app.bundle.js'
+     },
+     resolve: {
+       alias: {
+         'semantic-ui': path.join(__dirname, "node_modules", "semantic-ui-css", "semantic.js"),
+       },
+       extensions: ['', '.js', '.jsx']
+     },
+     module: {
+       loaders: [{
+         test: /\.jsx?$/,
+         loader: 'babel-loader'
+       }]
+     }
+ };
